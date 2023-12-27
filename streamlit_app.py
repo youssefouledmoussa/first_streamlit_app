3e7156a6f399706e5da038a2d79b1cbb091d2850
Youssef Ouled Moussa <154996660+youssefouledmoussa@users.noreply.github.com>
2023-12-27 16:53:01 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit 
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('My Omega3 & Blueberrie Oatmeal')
+streamlit.text('Kale,Spinach & Rocket Smoothie')
+streamlit.text('Hard-boiled Free-Ranged Egg')
