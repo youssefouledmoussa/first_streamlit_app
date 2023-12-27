c882bc9086469e300bebd32a406708d3a27a655d
Youssef Ouled Moussa <154996660+youssefouledmoussa@users.noreply.github.com>
2023-12-27 17:03:30 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,4 @@ streamlit.text('🐔 Hard-boiled Free-Ranged Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
