4670e63add61e8b769c69f66b042771518c7bd93
Youssef Ouled Moussa <154996660+youssefouledmoussa@users.noreply.github.com>
2023-12-27 17:01:00 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,4 @@ streamlit.text('🥗 Kale,Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-boiled Free-Ranged Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+import pandas
