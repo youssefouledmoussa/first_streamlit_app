f8f03f27e7f013c5f477285c64a48cffde2112cb
Youssef Ouled Moussa <154996660+youssefouledmoussa@users.noreply.github.com>
2023-12-29 22:18:20 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,9 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/fruit_choice")
+
+import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/kiwi")
 
 
