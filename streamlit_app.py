a3b8656068b3f1963360c233ea96ccc049459754
Youssef Ouled Moussa <154996660+youssefouledmoussa@users.noreply.github.com>
2023-12-29 21:46:11 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,3 +21,4 @@ streamlit.dataframe(my_fruit_list)
 fruits_selected = streamlit.multiselect("Pick some fruits:", list (my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
+streamlit.header("Fruityvice Fruit Advice!")
