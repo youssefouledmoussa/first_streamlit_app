b179910a25c66d7694c40f66c72ca9c0d9e46d02
Youssef Ouled Moussa <154996660+youssefouledmoussa@users.noreply.github.com>
2023-12-29 22:19:16 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,8 +30,7 @@ streamlit.write('The user entered ', fruit_choice)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/fruit_choice")
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/kiwi")
+
 
 
 # write your own comment -what does the next line do? 
