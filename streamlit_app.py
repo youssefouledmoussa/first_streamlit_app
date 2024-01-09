aad77d49741cffa73c57a24f920404fc3da95128
Youssef Ouled Moussa <154996660+youssefouledmoussa@users.noreply.github.com>
2024-01-09 19:17:07 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,3 +47,5 @@ my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
+
+conda install snowflake-snowpark-python
